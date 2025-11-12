5836c7fcdbb71bc6bd7e9d13b6a7070bea5c9662
BbyWiz <96n_lawrence@outlook.com>
2025-11-11 23:05:28 -0500

    Reduced swagger route options.

--- node_modules/server/routes/router.js
+++ node_modules/server/routes/router.js
@@ -1,8 +1,8 @@
 const express = require("express");
 const health = require("./health_route");
-const alpha = require("./alpha_route");
-const fmp = require("./fmp_route");
-const nasdaq = require("./nasdaq_route");
+// const alpha = require("./alpha_route");
+// const fmp = require("./fmp_route");
+// const nasdaq = require("./nasdaq_route");
 const path = require("path");
 const router = express.Router();
 const yahoo = require('./yahoo_route');
@@ -11,9 +11,9 @@ require("dotenv").config({ path: path.join(__dirname, "..", ".env") });
 
 
 router.use(health);
-router.use(alpha);
-router.use(fmp);
-router.use(nasdaq);
+// router.use(alpha);
+// router.use(fmp);
+// router.use(nasdaq);
 router.use(yahoo);
 
 module.exports = router;
